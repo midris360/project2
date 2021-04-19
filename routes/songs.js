d4ead2dc7bdc305eac5920460a843eb98ad56d51
Mohammed Idris <midris360@gmail.com>
2021-04-18 22:47:42 -0400

    updated routes, ejs

--- routes/songs.js
+++ routes/songs.js
@@ -5,6 +5,7 @@ const router = require("express").Router()
 const User = require("../models/User")
 const Songs = require("../models/Songs")
 const auth = require("./authMiddleware")
+const Song = require("../models/Songs")
 
 ///////////////////////////////
 // Router Specific Middleware
@@ -16,9 +17,43 @@ router.use(auth)
 ///////////////////////////////
 // Router Routes
 ////////////////////////////////
-router.get("/", (req, res) => {
-    res.render("home")
+router.get("/", async (req, res) => {
+    const songs = await Songs.find({user: req.session.user.id})
+        console.log(songs)
+    res.render("songs/index", {
+        songs
+    });
+});
+
+router.post("/", async (req, res) => {
+    req.body.user = req.session.user.id
+    await Song.create(req.body)
+    res.redirect("/songs/")
+});
+
+router.get("/:id", async (req, res) => {
+    const id = req.params.id
+    const dream = await Songs.findOne({_id: id, user: req.session.user.id})
+    if (song) {
+        res.render("songs/show", {
+            song
+        })
+    } else {
+        res.status(400).json({error: "No song of This ID for this user"})
+    }
 })
+
+router.put("/:id", async (req, res) => {
+    await Song.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect("/songs")
+})
+
+router.delete("/:id", async (req, res) => {
+    await Song.findByIdAndRemove(req.params.id)
+    res.redirect("/songs")
+})
+
+
 
 router.get("/dashboard", (req, res) => {
     res.render("dashboard")
